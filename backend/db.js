abfd61f81f9d0480c1f3395c452bb4ccedf8ecd0
Palash <palashrambhia2@gmail.com>
2024-08-07 12:56:32 +0530

    changes

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const mongoURI = `${process.env.DATABASE}`
-console.log(mongoURI.toString());
 // "mongodb://localhost:27017/inoted?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
 
 const connectToMongo = ()=>{
